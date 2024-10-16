b93155e14d9ec870331aca603534c0b5a66c1870
Sunil <sunilkr15012000@gmail.com>
2024-10-16 18:24:07 +0530

    2

--- server/index.js
+++ server/index.js
@@ -12,16 +12,16 @@ connectToMongo();
 
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://chat-bot-66lx.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 
 app.use(express.json());
