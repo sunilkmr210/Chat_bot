209f18034bf889ba437b0449a8ee785d45eca937
Sunil <sunilkr15012000@gmail.com>
2024-10-16 16:44:18 +0530

    2

--- server/index.js
+++ server/index.js
@@ -10,12 +10,14 @@ const app = express();
 const port = 5000;
 connectToMongo();
 
-app.use(express.json());
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/chats', chatRoute);
