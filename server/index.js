46ceb9dcef4a92bd621c85c421753bd610ea44a9
Sunil <sunilkr15012000@gmail.com>
2024-10-16 16:45:56 +0530

    2

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors()); 
+
 
 app.use(express.json());
 
