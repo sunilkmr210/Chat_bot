bd0566aff074621f222ee79e9b86c2ad0906f032
Sunil <sunilkr15012000@gmail.com>
2024-10-16 16:50:44 +0530

    2

--- server/index.js
+++ server/index.js
@@ -12,11 +12,16 @@ connectToMongo();
 
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://chat-bot-hp42.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors()); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://chat-bot-hp42.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.use(express.json());
